3db214cf8c3b166888e924663c06e95d5ed1e483
PDSamson <pdsamson72@gmail.com>
2018-05-25 09:58:27 -0400

    Changed styling to properly implement css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './App.css';
 import SignUpForm from './components/SignUpForm'
 import SignInForm from './components/SignInForm'
 import EventList from './components/EventList'
